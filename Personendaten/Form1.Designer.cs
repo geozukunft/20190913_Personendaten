3fa921aae6543344d30a2298ee5f75c9e263f574
Viktor Lennert <geozukunft@outlook.at>
2019-09-13 09:38:20 +0200

    Ja bissl was halt geschrieben

--- Personendaten/Form1.Designer.cs
+++ Personendaten/Form1.Designer.cs
@@ -28,13 +28,211 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastname = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.lblBirthYear = new System.Windows.Forms.Label();
+            this.numBirthyear = new System.Windows.Forms.NumericUpDown();
+            this.gboGender = new System.Windows.Forms.GroupBox();
+            this.rdoFemale = new System.Windows.Forms.RadioButton();
+            this.rdoMale = new System.Windows.Forms.RadioButton();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.cmdResult = new System.Windows.Forms.Button();
+            this.cmdClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numBirthyear)).BeginInit();
+            this.gboGender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblTitle.Location = new System.Drawing.Point(485, 46);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(522, 55);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Anzeige Personaldaten";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(98, 283);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(233, 55);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "Vorname:";
+            // 
+            // lblLastname
+            // 
+            this.lblLastname.AutoSize = true;
+            this.lblLastname.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblLastname.Location = new System.Drawing.Point(60, 373);
+            this.lblLastname.Name = "lblLastname";
+            this.lblLastname.Size = new System.Drawing.Size(271, 55);
+            this.lblLastname.TabIndex = 2;
+            this.lblLastname.Text = "Nachname:";
+            this.lblLastname.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtFirstName.Location = new System.Drawing.Point(361, 283);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(307, 62);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtLastname.Location = new System.Drawing.Point(361, 373);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(307, 62);
+            this.txtLastname.TabIndex = 4;
+            // 
+            // lblBirthYear
+            // 
+            this.lblBirthYear.AutoSize = true;
+            this.lblBirthYear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblBirthYear.Location = new System.Drawing.Point(43, 464);
+            this.lblBirthYear.Name = "lblBirthYear";
+            this.lblBirthYear.Size = new System.Drawing.Size(288, 55);
+            this.lblBirthYear.TabIndex = 5;
+            this.lblBirthYear.Text = "Geburtsjahr:";
+            // 
+            // numBirthyear
+            // 
+            this.numBirthyear.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.numBirthyear.Location = new System.Drawing.Point(361, 464);
+            this.numBirthyear.Maximum = new decimal(new int[] {
+            2019,
+            0,
+            0,
+            0});
+            this.numBirthyear.Minimum = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            this.numBirthyear.Name = "numBirthyear";
+            this.numBirthyear.Size = new System.Drawing.Size(185, 62);
+            this.numBirthyear.TabIndex = 6;
+            this.numBirthyear.Value = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            // 
+            // gboGender
+            // 
+            this.gboGender.Controls.Add(this.rdoFemale);
+            this.gboGender.Controls.Add(this.rdoMale);
+            this.gboGender.Location = new System.Drawing.Point(1008, 283);
+            this.gboGender.Name = "gboGender";
+            this.gboGender.Size = new System.Drawing.Size(306, 187);
+            this.gboGender.TabIndex = 7;
+            this.gboGender.TabStop = false;
+            this.gboGender.Text = "Geschlecht";
+            this.gboGender.Enter += new System.EventHandler(this.gboGender_Enter);
+            // 
+            // rdoFemale
+            // 
+            this.rdoFemale.AutoSize = true;
+            this.rdoFemale.Location = new System.Drawing.Point(43, 122);
+            this.rdoFemale.Name = "rdoFemale";
+            this.rdoFemale.Size = new System.Drawing.Size(225, 59);
+            this.rdoFemale.TabIndex = 1;
+            this.rdoFemale.TabStop = true;
+            this.rdoFemale.Text = "weiblich";
+            this.rdoFemale.UseVisualStyleBackColor = true;
+            // 
+            // rdoMale
+            // 
+            this.rdoMale.AutoSize = true;
+            this.rdoMale.Location = new System.Drawing.Point(43, 62);
+            this.rdoMale.Name = "rdoMale";
+            this.rdoMale.Size = new System.Drawing.Size(247, 59);
+            this.rdoMale.TabIndex = 0;
+            this.rdoMale.TabStop = true;
+            this.rdoMale.Text = "männlich";
+            this.rdoMale.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblResult.Location = new System.Drawing.Point(260, 723);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 55);
+            this.lblResult.TabIndex = 8;
+            // 
+            // cmdResult
+            // 
+            this.cmdResult.Location = new System.Drawing.Point(1008, 564);
+            this.cmdResult.Name = "cmdResult";
+            this.cmdResult.Size = new System.Drawing.Size(268, 86);
+            this.cmdResult.TabIndex = 9;
+            this.cmdResult.Text = "Bestätigen";
+            this.cmdResult.UseVisualStyleBackColor = true;
+            this.cmdResult.Click += new System.EventHandler(this.cmdResult_Click);
+            // 
+            // cmdClose
+            // 
+            this.cmdClose.Location = new System.Drawing.Point(1008, 668);
+            this.cmdClose.Name = "cmdClose";
+            this.cmdClose.Size = new System.Drawing.Size(268, 86);
+            this.cmdClose.TabIndex = 10;
+            this.cmdClose.Text = "Beenden";
+            this.cmdClose.UseVisualStyleBackColor = true;
+            this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(28F, 55F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1596, 974);
+            this.Controls.Add(this.cmdClose);
+            this.Controls.Add(this.cmdResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.gboGender);
+            this.Controls.Add(this.numBirthyear);
+            this.Controls.Add(this.lblBirthYear);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblLastname);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "Form1";
+            this.Text = "Personaldaten";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numBirthyear)).EndInit();
+            this.gboGender.ResumeLayout(false);
+            this.gboGender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastname;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.Label lblBirthYear;
+        private System.Windows.Forms.NumericUpDown numBirthyear;
+        private System.Windows.Forms.GroupBox gboGender;
+        private System.Windows.Forms.RadioButton rdoFemale;
+        private System.Windows.Forms.RadioButton rdoMale;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button cmdResult;
+        private System.Windows.Forms.Button cmdClose;
     }
 }
 
--- Personendaten/Form1.cs
+++ Personendaten/Form1.cs
@@ -12,9 +12,59 @@ namespace Personendaten
 {
     public partial class Form1 : Form
     {
+        private string gender;
         public Form1()
         {
             InitializeComponent();
+            gboGender.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gboGender_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdResult_Click(object sender, EventArgs e)
+        {
+            if(rdoFemale.Checked == true)
+            {
+                gender = rdoFemale.Text;
+                lblResult.BackColor = Color.Pink;
+            }
+            else if(rdoMale.Checked == true)
+            {
+                gender = rdoMale.Text;
+                lblResult.BackColor = Color.Blue;
+            }
+            else
+            {
+                gender = "An exception was thrown 0x000001";
+            }
+            lblResult.Text = "Sie heißen " + txtFirstName.Text + " " + txtLastname.Text +
+                "\nSie sind " + numBirthyear.Value + " geboren" +
+                "\nIhr Geschlecht ist " + gender;
+
+
+        }
+
+        private void cmdClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
